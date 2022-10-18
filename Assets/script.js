73b6fc7f07cbcdc05bc906cdd4b9d325cb35e5f7
Rj Saini <rsaini88@gmail.com>
2022-10-18 16:02:32 -0700

    Adder timer

---
+++ Assets/script.js
@@ -0,0 +1,33 @@
+/*when I click on start quiz, time starts and are displayed with questions to answer.
+when I click reset, it cancels the quiz.
+when i click view high score, it grants me score for local storage of my past scores
+score board shows the current questions I answered correctly
+
+
+
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+ */
+var time = document.querySelector("#time");
+var secondsLeft = 30;
+
+function timer() {
+  var timeInterval = setInterval(function () {
+    secondsLeft--;
+    time.textContent = secondsLeft;
+    if (secondsLeft === 0) {
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+}
+
+timer();
